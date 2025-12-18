166d859288ed959495286cab5a0621357feeb791
prashanth <kasamsaimaniprashanth@gmail.com>
2025-12-18 21:34:42 +0530

    initialize a vite+react app and added tailwind css

---
+++ README.md
@@ -0,0 +1,2 @@
+projects are for this yt video
+https://youtu.be/5ZdHfJVAY-s?si=vpMM1u8F-4X_9naN
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-green-500'>hello</div>
+    </>
+  )
+}
+
+export default App
