4a615d126717067124b43e8157f23b1867ea0347
prashanth <kasamsaimaniprashanth@gmail.com>
2025-12-24 22:44:29 +0530

    scroll indicator project is compleated

--- src/components/scroll-indicator/index.jsx
+++ src/components/scroll-indicator/index.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react"
+import './scroll.css'
 
 export default function ScrollIndicator({ url }) {
 
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
-    const [scrollPercentage, setScrollPercentage] = useState();
+    const [scrollPercentage, setScrollPercentage] = useState(0);
 
 
     async function fetchData(getUrl) {
@@ -33,19 +34,57 @@ export default function ScrollIndicator({ url }) {
         fetchData(url);
     }, [url]);
 
+    function handScrollPercentage() {
+        const howMuchScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+        setScrollPercentage((howMuchScroll / height) * 100)
+
+    }
+
+
+    useEffect(() => {
+        window.addEventListener('scroll', handScrollPercentage)
+
+        return () => {
+            window.removeEventListener('scroll', () => {
+
+            })
+        }
+    })
 
 
     console.log(data, loading);
-    return (
-        <div className="bg-black text-white flex justify-center items-center py-4 font-semibold">
-            <div>
 
+    if (errorMessage) {
+        return <div>error in loading data</div>
+    }
+
+    if (loading) {
+        return <div>loading data ! Please wait</div>
+    }
+
+    return (
+
+        <div className="bg-black text-white flex justify-center items-center py-4 font-medium">
+            <div className="top-container">
+                <h1>Custom Scroll Indicator</h1>
+                <div className="scroll-progress-tracking-container">
+                    <div
+                        className="current-progress-bar"
+                        style={{ width: `${scrollPercentage}%` }}
+                    ></div>
+                </div>
+            </div>
+
+            <div className="text-center py-8">
                 {data && data.length > 0 ?
                     data.map(dataItem => <p>{dataItem.title}</p>)
                     : null
-
                 }
             </div>
         </div>
+
+
     )
 }
