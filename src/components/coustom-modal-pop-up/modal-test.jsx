2a590952588c21d58ebb3eff56841c899dfddcd4
prashanth <kasamsaimaniprashanth@gmail.com>
2025-12-27 22:37:35 +0530

    css for modal pop is finished

--- src/components/coustom-modal-pop-up/modal-test.jsx
+++ src/components/coustom-modal-pop-up/modal-test.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Modal from "./modal";
-
+import './modal.css'
 
 const ModalTest = () => {
 
@@ -8,16 +8,19 @@ const ModalTest = () => {
 
     function handelModalTogglePopup() {
         setShowModalPopup(!showModalPopup)
+    };
+    function onClose() {
+        setShowModalPopup(false)
     }
 
     return (
         <div className="bg-black border-2 rounded-2xl text-white w-40 p-2.5 ">
             <button onClick={handelModalTogglePopup}>Open Modal popup</button>
             {
-                showModalPopup && <Modal
-                    body={
-                        "hi"
-                    }
+                showModalPopup && <Modal onClose={onClose}
+                    header={"coustom header"}
+                    body={"coustom body"}
+                    footer={"coustom footer"}
                 />
             }
         </div>
